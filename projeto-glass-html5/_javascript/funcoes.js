d808f0e051d33ba6ce67315315b5176b263557dd
NycolasR <nycolasramon3@gmail.com>
2020-06-17 16:36:18 -0300

    Ajustes feitos no menu principal

--- projeto-glass-html5/_javascript/funcoes.js
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -1,26 +1,3 @@
-let icone = document.getElementById('icone')
-let btnHome = document.getElementById('btnHome')
-let btnSpec = document.getElementById('btnSpec')
-let btnFotos = document.getElementById('btnFotos')
-let btnMultimidia = document.getElementById('btnMultimidia')
-let btnFaleConosco = document.getElementById('btnFaleConosco')
-
-/*btnHome.addEventListener('mouseout', restaurarIcone)
-btnSpec.addEventListener('mouseout', restaurarIcone)
-btnFotos.addEventListener('mouseout', restaurarIcone)
-btnMultimidia.addEventListener('mouseout', restaurarIcone)
-
-btnHome.addEventListener('mouseenter', trocarIcone('_imagens/home.png'))
-btnSpec.addEventListener('mouseenter', trocarIcone('_imagens/especificacoes-02.png'))
-
-btnFaleConosco.addEventListener('mouseout', restaurarIcone)
-
-*/
-
-function restaurarIcone() {
-    icone.src = "_imagens//glass-oculos-preto-peq.png"
-}
-
-function trocarIcone(path) {
-    icone.src = path
+function trocarIcone(foto) {
+   document.getElementById('icone').src = foto
 }
